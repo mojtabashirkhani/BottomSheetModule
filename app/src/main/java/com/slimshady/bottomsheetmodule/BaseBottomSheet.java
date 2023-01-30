b606f22d920341f0432c7934c5529a5650128227
Mojtaba Shirkhani <60153@sap.local>
2023-01-30 11:27:22 +0330

    add [BaseBottomSheet] class for adding another abstraction layer for general use

---
+++ app/src/main/java/com/slimshady/bottomsheetmodule/BaseBottomSheet.java
@@ -0,0 +1,71 @@
+package com.slimshady.bottomsheetmodule;
+
+
+import static com.slimshady.util.ScreenUtils.getWindowHeight;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.viewbinding.ViewBinding;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.slimshady.bottomsheetmodule.abstractions.AbstractBottomSheet;
+
+public class BaseBottomSheet<V extends ViewBinding> extends AbstractBottomSheet {
+
+    protected V viewBinding;
+    protected Context context;
+    protected int parentLayoutBottomSheetResId;
+
+    protected View viewBottomsheet;
+    protected BottomSheetBehavior bottomSheetBehavior;
+    protected int expandedHeight; //Height of bottom sheet in expanded state
+    protected int peekHeight;
+
+    public BaseBottomSheet(V viewBinding, Context context, int parentLayoutBottomSheetResId) {
+        this.viewBinding = viewBinding;
+        this.context = context;
+        this.parentLayoutBottomSheetResId = parentLayoutBottomSheetResId;
+
+        View view = viewBinding.getRoot();
+        viewBottomsheet = view.findViewById(parentLayoutBottomSheetResId);
+        bottomSheetBehavior = BottomSheetBehavior.from(viewBottomsheet);
+
+    }
+
+
+    @Override
+    protected void initBottomSheetView(Context context) {
+//        viewBottomsheet.setBackgroundResource(R.drawable.bottom_dialog_shape);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+        //Retrieve bottom sheet parameters
+        ViewGroup.LayoutParams bottomSheetLayoutParams = viewBottomsheet.getLayoutParams();
+        bottomSheetLayoutParams.height = getWindowHeight(context);
+
+        expandedHeight = bottomSheetLayoutParams.height;
+        peekHeight = (int) (expandedHeight / 2.3); //Peek height to 70% of expanded height (Change based on your view)
+
+        //Setup bottom sheet
+        viewBottomsheet.setLayoutParams(bottomSheetLayoutParams);
+        BottomSheetBehavior.from(viewBottomsheet).setSkipCollapsed(false);
+        BottomSheetBehavior.from(viewBottomsheet).setPeekHeight(peekHeight);
+        BottomSheetBehavior.from(viewBottomsheet).setHideable(true);
+
+    }
+
+    @Override
+    public void closeBottomSheet() {
+        if (bottomSheetBehavior != null) {
+            if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED ||
+                    (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) &&
+                            bottomSheetBehavior.getPeekHeight() != 0) {
+                bottomSheetBehavior.setPeekHeight(0);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/slimshady/util/ScreenUtils.java
@@ -0,0 +1,13 @@
+package com.slimshady.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+public class ScreenUtils {
+    public static int getWindowHeight(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+}
