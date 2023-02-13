4842737d39b75c090bf9289b6b99c27582cb4686
Mojtaba Shirkhani <60153@sap.local>
2023-02-13 15:56:22 +0330

    change kotlin activity to java file

---
+++ app/src/main/java/com/slimshady/MainActivity.java
@@ -0,0 +1,16 @@
+package com.slimshady;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.slimshady.bottomsheetmodule.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
